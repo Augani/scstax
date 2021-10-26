6dce8bc556380507fb6ff9568c7f5d1098609c8b
Augustus <augani7@gmail.com>
2021-10-26 21:57:39 +0000

    added tailwindcss to project

---
+++ components/companySection/index.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+
+
+
+function Index(): ReactElement {
+    return (
+        <div className="">
+            
+        </div>
+    )
+}
+
+export default Index
